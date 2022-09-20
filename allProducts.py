ccf4f79691522e736cf1c72fbcc5b2bb8bc80b29
Xavier Kidston <kidstonx@gmail.com>
2022-09-19 23:51:15 -0600

    Changed from API to web scrape.
    Moved from API (due to cost) to web scrape. Changed variable names accordingly, and finalized the webscrape to excel sheet pipeline.

--- allProducts.py
+++ allProducts.py
@@ -1,42 +1,66 @@
 import product
 from os.path import exists
+from product import webdriver
+from datetime import date
 
-
-class amazonChecking:
+class amazonProducts:
     def __init__(self, fileName="products.xlsx"):
         self.fileName = fileName
+        self.products = {}
         if exists(self.fileName):                                   # Loads spreadsheet
             self.wb = product.openpyxl.load_workbook(self.fileName)
+            self.__loadExcel__()
         else:                                                       # Creates workbook and spreadsheet filling it with format
             self.wb = product.openpyxl.Workbook()
             self.wb.worksheets[0].title = "Products"
             self.wb.worksheets[0].cell(2,1).value = "Names: "
-            self.wb.worksheets[0].cell(3,1).value = "ASIN: "
+            self.wb.worksheets[0].cell(3,1).value = "URI: "
             self.wb.worksheets[0].cell(1,2).value = "Dates: "
-        self.wb.save(filename=self.fileName)
+            self.wb.save(filename=self.fileName)
 
-    def addProduct(self, name, asin):
-        product.product(name, asin, self.wb.worksheets[0])
-        self.wb.save(filename=self.fileName)
+    def __loadExcel__(self):
+        lis = [list(r) for r in self.wb.worksheets[0].iter_rows(values_only=True, min_col=3, max_row=2, min_row=2)]
+        for column in self.wb.worksheets[0].iter_cols(min_row=2, min_col=3):
+            self.addProduct(column[0].value, column[1].value, lis)
+
+    def addProduct(self, title, uri, keys=None):
+        if title not in self.products.keys():
+            item = product.product(title, uri, self.wb.worksheets[0], keys)
+            self.products[title] = item
+            self.wb.save(filename=self.fileName)
 
     def remProduct(name):
         pass
 
-    def updatePricing(self):
-        ASINs = self.wb.worksheets[0].iter_cols(min_row=3, max_row=3, min_col=3, values_only=True)
-        for ASIN in ASINs:
-            print(ASIN)
+    async def updatePricing(self):
+        opts = webdriver.chrome.options.Options()
+        opts.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+        opts.add_argument('--ignore-certificate-errors')
+        opts.add_argument("--disable-extensions")
+        opts.add_argument("--headless")
+        opts.add_argument("--disable-webgl")
+        opts.add_argument("--disable-gpu")
+        #Gets the page open
+        driver = webdriver.Chrome(executable_path = "chromedriver", options = opts)
 
+        for product in self.products.values():
+            await product.getPriceFromPage(driver)
+        
+        self.wb.save(filename=self.fileName)
+        self.wb.worksheets[0].cell(self.wb.worksheets[0].max_row + 1, 2).value = date.today()       # Sets the date for today in excel
+    
     def __str__(self):
-        products = self.wb.worksheets[0].iter_cols(min_row=2, max_row=3, min_col=3, values_only=True)
-        for product in products:
-            print(product)
-        return ""
+        string = ""
+        for product in self.products.values():
+            string += str(product)
+            string += "\n"
+        return string
 
     
 if __name__ == "__main__":
-    x = amazonChecking()
-    x.addProduct("Samsung", "22fd2")
-    x.addProduct("Vivo", "3fd5")
+    x = amazonProducts()
+    x.addProduct("Samsung 49\" Odyssey Neo g9", "https://www.amazon.ca/SAMSUNG-5120x1440-Adjustable-FreeSync-LS49AG952NNXZA/dp/B096YNP6ZR/ref=sr_1_2?crid=KUUFTHC5SA5R&keywords=samsung+odyssey+g9+neo&qid=1663031908&sprefix=samsung+odyssey+g9+neo%2Caps%2C107&sr=8-2")
+    x.addProduct("Vivo Premium Heavy Duty Monitor Arm", "https://www.amazon.ca/VIVO-Premium-Aluminum-Pneumatic-STAND-V101G1/dp/B07L8MLTGS/ref=sr_1_5?keywords=vivo+pneumatic+monitor+arm&qid=1663031948&sprefix=vivo+pneumat%2Caps%2C130&sr=8-5")
     print(x)
-    x.updatePricing()
+    import asyncio
+    asyncio.run(x.updatePricing())
--- product.py
+++ product.py
@@ -1,36 +1,73 @@
 import openpyxl
-
+from selenium import webdriver
 class product:
-    # Defines perameters necessary for searching in API
-    def __init__(self, title, ASIN, excelSheet):
+    
+    def __init__(self, title, URI, excelSheet, keys=None):
         self.title = title
         self.sheet = excelSheet
-        self.ASIN = ASIN
+        self.URI = URI
         self.col = self.sheet.max_column+1
-        self.sheet.cell(2, self.col).value = self.title
-        self.sheet.cell(3, self.col).value = self.ASIN
+        print(keys)
+        if keys == None or title not in keys[0]:
+            self.sheet.cell(2, self.col).value = self.title
+            self.sheet.cell(3, self.col).value = self.URI
 
-    # Incase you need to update the ASIN
-    def setASIN(self, ASIN):
-        self.ASIN = ASIN
-        self.sheet.cell(3, self.col).value = ASIN
+    # Purpose: Sets URI in case it must be updated\
+    # Params:  URI - The uri of the of the item
+    def setURI(self, URI):
+        self.URI = URI
+        self.sheet.cell(3, self.col).value = URI
     
-    # Calls the amazon API to get the price with the ASIN
-    def currPrice(self, price):
-        self.sheet.cell(self.sheet.max_row, self.col).value = price
+    # Purpose: To input a price directyl
+    # Usage:   Used when recreating already existing entries from excel
+    def setPrice(self, price):
+        self.price = price
 
-    # Returns title and ASIN
+    # Gets the price in the excel sheet
+    def currPrice(self):
+        return self.price
+
+    async def getPriceFromPage(self, driver=webdriver):
+        driver.get(self.URI)
+        result = None
+        while result == None:
+            try:
+                self.price = driver.find_element(webdriver.common.by.By.CLASS_NAME, "a-price-whole").text
+                result = 1
+            except: pass
+        self.price+= "."
+        while result == 1:
+            try:
+                self.price+= driver.find_element(webdriver.common.by.By.CLASS_NAME, "a-price-fraction").text
+                result = 2
+            except: pass
+        self.sheet.cell(self.sheet.max_row, self.col).value = self.price
+        return
+
+    # Returns title and URI
     def __str__(self):
-        return "Name: " + self.title, "ASIN: " + self.ASIN + "\n"
+        string = "Title: "
+        string+= self.title
+        string+= "\nURI: "
+        string+= self.URI
+        string+= "\n"
+        return string
 
 
 if __name__ == "__main__":
     excelBook = openpyxl.Workbook()
     excelBook.worksheets[0].title = "Data"
     excelBook.worksheets[0].cell(2,1).value = "Titles: "
-    excelBook.worksheets[0].cell(3,1).value = "ASIN: "
+    excelBook.worksheets[0].cell(3,1).value = "URI: "
     excelBook.worksheets[0].cell(1,2).value = "Dates"
     print(excelBook.worksheets[0].max_column)
     p1 = product("AHHHHHHH", excelBook.worksheets[0])
-    p1.setASIN("baltimores")
+    p1.setURI("baltimores")
     excelBook.save(filename="aTest.xlsx")
+
+    options = webdriver.Options()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument("--window-size=200,200")
+    #Gets the page open
+    driver = webdriver.Chrome(executable_path = "chromedriver", chrome_options = options)
+    driver.get(URI)
